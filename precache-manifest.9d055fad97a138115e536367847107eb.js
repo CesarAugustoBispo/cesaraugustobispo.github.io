6a614218bcf0cb3f1232933d044176eac514ca69
César Bispo <dm.cesaraugusto@gmail.com>
2020-02-04 18:22:11 -0300

    alterações no portfolio

---
+++ precache-manifest.9d055fad97a138115e536367847107eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724e99f48e97a233a673179ac7022e28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e61ca30eea7c19b6f84",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "18359f8cb5d2e441f4d8",
+    "url": "/static/js/2.97f10662.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.97f10662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e61ca30eea7c19b6f84",
+    "url": "/static/js/main.052e97e3.chunk.js"
+  },
+  {
+    "revision": "784f934da7cae9e13356",
+    "url": "/static/js/runtime-main.1a63ad0f.js"
+  }
+]);
